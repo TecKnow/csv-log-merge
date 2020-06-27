baa3067adac3c80291959d396ea01439d66f277d
David Perkins <david.perkins@grumbleware.com>
2020-06-27 14:14:07 -0700

    Packaging (#4)
    Reshapes the project's structure to match best practices for a Python module and adds a `setup.py` file with reasonable defaults.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cperkins-CSV-log", # Replace with your own username
+    version="0.0.1",
+    author="Casey Perkins",
+    author_email="perkinscc07@gmail.com",
+    description="A package for managing multiple CSV log files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TecKnow/cperkins-csv-log",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        "console_scripts": [],
+    },
+)
