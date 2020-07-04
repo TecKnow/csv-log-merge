7c943c7b1266d8b89e35876d1724709f9bea0b00
David P. Perkins <tecknow@grumbleware.com>
2020-07-03 21:42:43 -0500

    Adds tests for 'cvslog.cvs_merge.log_record_combiner'

--- tests/test_csv_merge.py
+++ tests/test_csv_merge.py
@@ -1,42 +1,116 @@
+import csv
 import os
+from io import StringIO
 from pathlib import Path
+from typing import Iterator, Sequence, Tuple
 
 import pytest
 
-from csvlog.csv_merge import get_csv_paths_in_directory
+from csvlog.csv_merge import get_csv_paths_in_directory, log_record_combiner
 
 
-class TestCSVMerge:
-    def test_get_csv_paths_in_directory_defaults(self, csv_paths_temp_dir):
+class TestGetCSVPathsInDirectory:
+    def test_defaults(self, csv_paths_temp_dir):
         res = set(p.stem for p in get_csv_paths_in_directory(Path.cwd()))
+        assert Path.cwd().samefile(csv_paths_temp_dir)
         assert res == set("one two three ignore".split())
 
-    def test_get_csv_paths_in_directory_not_in_cwd(self, csv_paths_temp_dir):
+    def test_not_in_cwd(self, csv_paths_temp_dir):
         outside_dir = Path(csv_paths_temp_dir, "other_dir")
         outside_dir.mkdir()
         os.chdir(outside_dir)
         res = set(p.stem for p in get_csv_paths_in_directory(csv_paths_temp_dir))
         assert res == set("one two three ignore".split())
 
-    def test_get_csv_paths_in_directory_defaults_recursive(self, csv_paths_temp_dir):
-        res = set(p.stem for p in get_csv_paths_in_directory(Path.cwd(), recurse=True))
+    def test_defaults_recursive(self, csv_paths_temp_dir):
+        res = set(p.stem for p in get_csv_paths_in_directory(csv_paths_temp_dir, recurse=True))
         assert res == set("one two three four five ignore".split())
 
-    def test_get_csv_paths_in_directory_recursive_not_in_cwd(self, csv_paths_temp_dir):
+    def test_recursive_not_in_cwd(self, csv_paths_temp_dir):
         outside_dir = Path(csv_paths_temp_dir, "other_dir")
         outside_dir.mkdir()
         os.chdir(outside_dir)
         res = set(p.stem for p in get_csv_paths_in_directory(csv_paths_temp_dir, recurse=True))
         assert res == set("one two three four five ignore".split())
 
-    def test_get_csv_paths_in_directory_with_ignore(self, csv_paths_temp_dir):
+    def test_with_ignore(self, csv_paths_temp_dir):
         res = set(p.stem for p in get_csv_paths_in_directory(Path.cwd(), ignore=Path(csv_paths_temp_dir, "ignore.csv")))
         assert res == set("one two three".split())
 
-    def test_get_csv_paths_in_directory_with_ignore_recursive(self, csv_paths_temp_dir):
+    def test_ignore_recursive(self, csv_paths_temp_dir):
         res = set(p.stem for p in
                   get_csv_paths_in_directory(Path.cwd(), recurse=True, ignore=Path(csv_paths_temp_dir, "ignore.csv")))
         assert res == set("one two three four five".split())
+
+
+class TestMoveFilesToArchive:
+    pass
+
+
+class TestLogRecordCombiner:
+    def test_no_header(self):
+        readers, writer, stream = self.csv_object_setup((NAME_ROWS, PLACES_ROWS))
+        function_returns = tuple(log_record_combiner(writer, reader) for reader in readers)
+        assert function_returns == (True, True)
+        assert tuple(csv.reader(stream.getvalue().splitlines())) == (*NAME_LIST, *PLACES_LIST)
+
+    def test_correct_headers(self):
+        names_with_headers = [HEADER_ROW] + NAME_ROWS
+        places_with_headers = [HEADER_ROW] + PLACES_ROWS
+        expected_results = (*NAME_LIST, *PLACES_LIST)
+        readers, writer, stream = self.csv_object_setup((names_with_headers, places_with_headers))
+        function_returns = tuple(log_record_combiner(writer, reader, HEADER_LIST) for reader in readers)
+        assert function_returns == (True, True)
+        assert tuple(csv.reader(stream.getvalue().splitlines())) == expected_results
+
+    def test_partial_incorrect_headers(self):
+        names_with_headers = [HEADER_ROW] + NAME_ROWS
+        places_with_bad_headers = [BAD_HEADER_ROW] + PLACES_ROWS
+        expected_results = (*NAME_LIST,)
+        readers, writer, stream = self.csv_object_setup((names_with_headers, places_with_bad_headers))
+        function_returns = tuple(log_record_combiner(writer, reader, HEADER_LIST) for reader in readers)
+        assert function_returns == (True, False)
+        assert tuple(csv.reader(stream.getvalue().splitlines())) == expected_results
+
+    def test_total_incorrect_headers(self):
+        names_with_bad_headers = [BAD_HEADER_ROW] + NAME_ROWS
+        places_with_bad_headers = [BAD_HEADER_ROW] + PLACES_ROWS
+        expected_results = tuple()
+        readers, writer, stream = self.csv_object_setup((names_with_bad_headers, places_with_bad_headers))
+        function_returns = tuple(log_record_combiner(writer, reader, HEADER_LIST) for reader in readers)
+        assert function_returns == (False, False)
+        assert tuple(csv.reader(stream.getvalue().splitlines())) == expected_results
+
+    @staticmethod
+    def csv_object_setup(data_sets: Iterator[Sequence[str]]) -> Tuple[Iterator[csv.reader], csv.writer, StringIO]:
+        readers = (csv.reader(data) for data in data_sets)
+        stream = StringIO()
+        writer = csv.writer(stream)
+        return readers, writer, stream
+
+
+class TestLogFileCombiner:
+    pass
+
+
+class TestMergeLogFiles:
+    pass
+
+
+HEADER_LIST = "ALPHA BRAVO CHARLIE DELTA ECHO".split()
+HEADER_ROW = ",".join(HEADER_LIST)
+BAD_HEADER_LIST = "ALPHA, BETA, GAMMA, DELTA, EPSILON".split()
+BAD_HEADER_ROW = ",".join(BAD_HEADER_LIST)
+NAME_LIST = [
+    "Alice Betty Christine Diana Erica".split(),
+    "Adam Bob Christopher Daniel Eugene".split(),
+    "Adams Bowers Cooper Davies Erickson".split()]
+NAME_ROWS = [",".join(row) for row in NAME_LIST]
+
+PLACES_LIST = [
+    "Alabama Alaska Arizona Arkansas California".split(),
+    "Atlanta Boston Chicago Davenport Evanston".split()]
+PLACES_ROWS = [",".join(row) for row in PLACES_LIST]
 
 
 @pytest.fixture
